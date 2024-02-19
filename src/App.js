5471e359f68b0cca9cd758dc3f079859a6dd5811
sumit khobragade <sumitbk1010@gmail.com>
2024-02-19 17:50:25 +0530

    ticket system completion

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<HomePage />} />
-
         <Route path="/form" element={<TicketForm />} />
       </Routes>
       {/* <MyForm /> */}
--- src/ticketform.js
+++ src/ticketform.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -8,18 +8,20 @@ const ContactForm = () => {
       "edbsn961867d6876142915d1a78b75cab80301cb4934c2e66419a39e7825e8d0f07fc",
     xJdfEaS: "",
     actionType: "Q2FzZXM=",
-    returnURL: "https&#x3a;&#x2f;&#x2f;main-app-7qi.pages.dev&#x2f;",
-    firstname: "",
-    lastname: "",
-    email: "",
-    subject: "",
-    description: "",
+    ReturnURL: "*",
+    "First Name": "",
+    "Contact Name": "",
+    Email: "",
+    Subject: "",
+    Description: "",
   });
-  // const options = {
-  //   method: "POST",
-  //   url: "https://desk.zoho.in/support/WebToCase",
-  //   params: formData,
-  // };
+  // ! Axios method
+  const options = {
+    method: "POST",
+    url: "https://desk.zoho.in/support/WebToCase",
+    params: formData,
+  };
+  // ! Axios method
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -31,49 +33,60 @@ const ContactForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // axios
-    //   .request(options)
-    //   .then(function (response) {
-    //     console.log("response", response.data);
-    //   })
-    //   .catch(function (error) {
-    //     console.error(error);
-    //   });
-    const options = {
-      method: "POST",
-      headers: {
-        cookie:
-          "2eed0b67fd=f369298bdcaa504163502a5596fe31d7; crmcsr=8c423b47-abaa-4edf-8a61-6797c7b74d50; _zcsr_tmp=8c423b47-abaa-4edf-8a61-6797c7b74d50",
-        "User-Agent": "insomnia/2023.5.8",
-      },
-    };
+    // ! Axios method
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log("response", response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    // ! Axios method
 
-    fetch(
-      `https://desk.zoho.in/support/WebToCase?xnQsjsdp=edbsn1d8d2e6bed3c705918f0969177cffca2&xmIwtLD=edbsn961867d6876142915d1a78b75cab80301cb4934c2e66419a39e7825e8d0f07fc&xJdfEaS=&actionType=Q2FzZXM%3D&First%20Name=${encodeURIComponent(
-        formData.firstname
-      )}&Contact%20Name=${encodeURIComponent(
-        formData.contactName
-      )}&Email=${encodeURIComponent(
-        formData.email
-      )}&Subject=${encodeURIComponent(
-        formData.subject
-      )}&Description=${encodeURIComponent(formData.description)}`,
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => console.log(response))
-      .catch((err) => console.error(err));
-    // You can handle the form submission logic here
+    // ! Fetch Method
+    // const options = {
+    //   method: "POST",
+    //   headers: {
+    //     cookie:
+    //       "2eed0b67fd=f369298bdcaa504163502a5596fe31d7; crmcsr=8c423b47-abaa-4edf-8a61-6797c7b74d50; _zcsr_tmp=8c423b47-abaa-4edf-8a61-6797c7b74d50",
+    //     "User-Agent": "insomnia/2023.5.8",
+    //   },
+    // };
+
+    // fetch(
+    //   `https://desk.zoho.in/support/WebToCase?xnQsjsdp=${
+    //     formData.xnQsjsdp
+    //   }&xmIwtLD=${formData.xmIwtLD}&xJdfEaS=${formData.xJdfEaS}&actionType=${
+    //     formData.actionType
+    //   }&First%20Name=${encodeURIComponent(
+    //     formData["First Name"]
+    //   )}&Contact%20Name=${encodeURIComponent(
+    //     formData["Contact Name"]
+    //   )}&Email=${encodeURIComponent(
+    //     formData.Email
+    //   )}&Subject=${encodeURIComponent(
+    //     formData.Subject
+    //   )}&Description=${encodeURIComponent(formData.Description)}`,
+    //   options
+    // )
+    //   .then((response) => response.json())
+    //   .then((response) => console.log(response))
+    //   .catch((err) => console.error(err));
+    // ! Fetch Method
+
     console.log("Form submitted:", formData);
+    alert("ticket is submitted");
+    handleReset();
   };
 
   const handleReset = () => {
     setFormData({
-      firstname: "",
-      lastname: "",
-      email: "",
-      subject: "",
-      description: "",
+      "First Name": "",
+      "Contact Name": "",
+      Email: "",
+      Subject: "",
+      Description: "",
     });
   };
 
@@ -86,8 +99,8 @@ const ContactForm = () => {
         First Name:
         <input
           type="text"
-          name="firstname"
-          value={formData.firstname}
+          name="First Name"
+          value={formData["First Name"]}
           onChange={handleChange}
           placeholder="First Name"
           required
@@ -98,8 +111,8 @@ const ContactForm = () => {
         Last Name:
         <input
           type="text"
-          name="lastname"
-          value={formData.lastname}
+          name="Contact Name"
+          value={formData["Contact Name"]}
           onChange={handleChange}
           required
           placeholder="Last Name"
@@ -110,8 +123,8 @@ const ContactForm = () => {
         Email:
         <input
           type="email"
-          name="email"
-          value={formData.email}
+          name="Email"
+          value={formData.Email}
           onChange={handleChange}
           placeholder="Email"
           required
@@ -122,8 +135,8 @@ const ContactForm = () => {
         Subject:
         <input
           type="text"
-          name="subject"
-          value={formData.subject}
+          name="Subject"
+          value={formData.Subject}
           onChange={handleChange}
           placeholder="Subject"
           required
@@ -133,8 +146,8 @@ const ContactForm = () => {
       <label>
         Description:
         <textarea
-          name="description"
-          value={formData.description}
+          name="Description"
+          value={formData.Description}
           onChange={handleChange}
           placeholder="Description"
           required
